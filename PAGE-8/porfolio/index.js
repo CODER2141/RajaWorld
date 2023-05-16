1755158dc4ff3de6d287d2dfbe85e69db31df33a
Rajan <131521582+CODER2141@users.noreply.github.com>
2023-05-17 04:31:37 +1030

    Add files via upload

---
+++ PAGE-8/porfolio/index.js
@@ -0,0 +1,28 @@
+$(Document).ready(function(){
+
+ $('#menu').click(function(){
+ $(this).toggleclass('fa-times');
+ $('header').toggleclass('toggle');
+ });
+
+ $(window).on('scroll load',function(){
+    $('#menu').removeclass('fa-times');
+    $('header').removeclass('toggle');
+ });
+
+
+ $('a[href'="#").on('click',function(e){
+   e.preventdefault();
+   $('html, body').animate({
+      scrollTop:$($(this).attr('href')).offset().top,
+   },
+   500,
+   'linear'
+   );
+
+});
+
+});
+
+ //smooth scrolling
+
